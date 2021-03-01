146d25a45e043657eaa40a0bdc66c5e47d26f221
andrea <corro.andrea.1995@gmail.com>
2021-03-01 13:39:44 +0100

    init

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+  $dischi = [
+    [
+      'titolo' => 'film Uno',
+      'anno' => '1992',
+      'genere' => 'azione',
+      'copertina' =>
+    ],
+    [
+      'titolo' => 'film due',
+      'anno' => '1990',
+      'genere' => 'thriller',
+      'copertina' =>
+    ],
+  ]
+ ?>
